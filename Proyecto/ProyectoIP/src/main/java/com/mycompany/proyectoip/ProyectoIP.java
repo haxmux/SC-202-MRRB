2b0b0668f14faafd2982698dfaf29812148fd4d2
haxmux <melrb2012@gmail.com>
2025-02-14 15:29:59 -0600

    Proyecto

---
+++ Proyecto/ProyectoIP/src/main/java/com/mycompany/proyectoip/ProyectoIP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoip;
+
+/**
+ *
+ * @author melrb
+ */
+public class ProyectoIP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
